d91a130eafc67069aae48481ec6998b465f02dd0
ABDELHAMID RAHMAOUI <rahmaoui.developer@gmail.com>
2022-11-06 21:20:52 +0100

    update working tree pattern

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -15,7 +15,7 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder("gomobile_nosix");
+        $treeBuilder = new TreeBuilder("gomobile_n_osix");
         $treeBuilder->getRootNode()
             ->children()
             ->scalarNode("login")->defaultValue("")->end()
--- src/DependencyInjection/GomobileNOsixExtension.php
+++ src/DependencyInjection/GomobileNOsixExtension.php
@@ -22,9 +22,9 @@ class GomobileNOsixExtension extends Extension
         $config = $this->processConfiguration($configuration, $configs);
 
         // Set parameters
-        $container->setParameter("gomobile_nosix.login", $config["login"]);
-        $container->setParameter("gomobile_nosix.password", $config["password"]);
-        $container->setParameter("gomobile_nosix.is_local", $config["is_local"]);
+        $container->setParameter("gomobile_n_osix.login", $config["login"]);
+        $container->setParameter("gomobile_n_osix.password", $config["password"]);
+        $container->setParameter("gomobile_n_osix.is_local", $config["is_local"]);
 
         $loader = new YamlFileLoader($container, new FileLocator(__DIR__ . "/../Resources/config"));
         $loader->load("services.yaml");
