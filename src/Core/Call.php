c3522cee5ca8911b68dc1266042888292627b6e7
ABDELHAMID RAHMAOUI <rahmaoui.developer@gmail.com>
2022-11-07 13:41:06 +0100

    fix bug error

--- src/Core/Call.php
+++ src/Core/Call.php
@@ -146,7 +146,7 @@ class Call extends Base
             else
                 return $this->error($result->status, $result->message);
         } else {
-            return $this->error("error while processing");
+            return $this->error(-10, "error while processing");
         }
     }
 
